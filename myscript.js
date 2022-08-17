290056f4e2ae515a3e4513143cec1255930c2aee
mamyh <likhmaon@gmail.com>
2022-08-17 06:35:22 +0600

    initially 1st project complete

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 //varriable initializing 
 
-const {parent,counter,increment,decrement,addCounter,reset} = ['parent','counter', 'increment','decrement','addCounter','reset'].reduce((elements,item)=>{
+const {parent,counter,addCounter,reset} = ['parent','counter', 'increment','decrement','addCounter','reset'].reduce((elements,item)=>{
       return {
         ...elements,
         [item]:document.querySelector(`.${item}`)
@@ -16,7 +16,7 @@ const initialValue ={
 }
 
 function nextId(counters){
-    const maxId = counters.reduce((max, id)=>Math.max(max,id),-1);
+    const maxId = counters.reduce((max, item)=>Math.max(max,item.id),-1);
     return maxId+1;
 }
 function identifer(){
@@ -24,7 +24,8 @@ function identifer(){
 }
 
 
-const counterReducer=(state=initialValue, {type,payload})=>{
+const counterReducer=(state=initialValue, {type,payload={}})=>{
+   console.log('i am from reducer');
     switch(type){
         case 'increment':{
           return{
@@ -68,6 +69,17 @@ const counterReducer=(state=initialValue, {type,payload})=>{
             ]
            }
         }
+        case 'reset':{
+          return {
+            ...state,
+            counter:state.counter.map(item=>{
+              return{
+                ...item,
+                value:0
+              }
+            })
+          }
+        }
         default:
           return {...state}
     }
@@ -77,16 +89,14 @@ const store= Redux.createStore(counterReducer);
 
 function render(){
   const state=store.getState();
-  
-  
-console.log(state);
-state.counter.forEach(item=>{
-  const counterEl =`<div class="mx-auto max-w-md mt-10  space-y-5 ">
+  let length=state.counter.length;
+parent.innerHTML=state.counter.map((item)=>{
+  return `<div class="mx-auto max-w-md mt-10  space-y-5 ">
                     <div
-                        data-id =${item.id} class=" max-w-md p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow "
+                        data-id ="${item.id.toString()}" class="findDom max-w-md p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow "
                       >
 
-                    <div  class="text-2xl font-semibold counter">${item.value}</div>
+                    <div  class="text-2xl font-semibold counter">${item.value.toString()}</div>
                     <div class="flex space-x-3">
                         <button
                             class="bg-indigo-400 text-white px-3 py-2 rounded shadow increment"  data-id =${item.id} id=${item.identity[0]}  
@@ -102,7 +112,6 @@ state.counter.forEach(item=>{
                   </div>
   
   </div>`;
-    parent.insertAdjacentHTML('beforeend',counterEl);
    
 });
 state.counter.forEach((item)=>{
@@ -133,21 +142,16 @@ state.counter.forEach((item)=>{
 
 render();
 
-function action(type,value=0){
-  return {
-    type,payload:value
-  }
-}
-
-function click(element,type){
-  element.addEvenetlistener('click',()=>{
-    store.dispatch(action(type,5))
-  });
-}
-
-
-
-
+addCounter.addEventListener('click',()=>{
+  store.dispatch({
+    type:'addcounter',
+  })
+});
+reset.addEventListener('click',()=>{
+  store.dispatch({
+    type:'reset'
+  })
+})
 store.subscribe(render);
 
 
